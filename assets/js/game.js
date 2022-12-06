84b6aacaf1ea69617ab06cf10162e52fd30bb0d8
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-12-06 21:34:58 +0600

    designed UI for game

---
+++ assets/js/game.js
@@ -0,0 +1,7 @@
+const dom = {
+	container: document.getElementById("game-container"),
+	character: document.getElementById("character"),
+	characterNameBox: document.getElementById("dialogue-character-name"),
+	dialogueBox: document.getElementById("dialogue"),
+	optionBox: document.getElementById("option-box"),
+};
