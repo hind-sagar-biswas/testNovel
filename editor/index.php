649f3f19c23c2514e0b71a35c961688dd1f955f3
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-12-10 05:31:30 +0600

    setup required files

---
+++ editor/index.php
@@ -0,0 +1,5 @@
+<?php 
+$PAGE_TITLE = 'TestNovel Editor';
+require __DIR__ . '/templates/includes/header.php';
+
+require __DIR__ . '/templates/includes/footer.php';
---
+++ editor/templates/includes/footer.php
@@ -0,0 +1,5 @@
+    
+    <script src='<?php echo __DIR__ . "/node_modules/@popperjs/core/dist/cjs/popper.js"; ?>'></script>
+    <script src='<?php echo __DIR__ . "/node_modules/bootstrap/dist/js/bootstrap.min.js"; ?>'></script>
+    </body>
+</html>
